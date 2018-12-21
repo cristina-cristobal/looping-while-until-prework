deafbfb66f820fca6c8eb9deaed86ae799a1b6dd
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-21 17:07:10 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -5,3 +5,11 @@ def using_until
   levitation_force += 1
 
 end
+
+def using_until 
+  levitation force = 6
+  until levitation_force == 10
+    puts "Wingardium Leviosa"
+    levitation_force += 1
+  end
+end 
